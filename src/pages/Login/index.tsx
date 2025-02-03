5eda3e51bfec5536411069f300ade1fc4f228449
Yasmin Zanon Barg <yasmin.barg@hotmail.com>
2025-02-03 17:46:00 -0300

    styled material design

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,17 +1,14 @@
-import "@material/web/textfield/outlined-text-field.js"
-import "@material/web/icon/icon.js"
-import "@material/web/iconbutton/icon-button.js"
-import '@material/web/button/filled-button.js'
+import "@material/web/textfield/outlined-text-field.js";
+import "@material/web/icon/icon.js";
+import "@material/web/iconbutton/icon-button.js";
+import '@material/web/button/filled-button.js';
 
-import { Container, ImageContainer, TextContainer } from "./styles"
-
-import backgroundLogin from '../../assets/background_login.png'
-
+import { Container, ImageContainer, TextContainer, OutlinedTextFieldStyled, FilledButtonStyled } from "./styles";  // Importando os componentes com styled
+import backgroundLogin from '../../assets/background_login.png';
 
 export function Login() {
     return(
         <Container>
-
             <TextContainer>
                 <div>
                     <h1>Eletrônica</h1>
@@ -19,30 +16,32 @@ export function Login() {
                 </div>
 
                 <form>
-                    <md-outlined-text-field
+                    <OutlinedTextFieldStyled
                         label="Username"
                         error
                         error-text="Username is not available"
                         required
                     >
-                    <md-icon slot="trailing-icon">error</md-icon>
-                    </md-outlined-text-field>
+                        <md-icon slot="trailing-icon">error</md-icon>
+                    </OutlinedTextFieldStyled>
 
-                    <md-outlined-text-field label="Password" type="password">
-                    <md-icon-button toggle slot="trailing-icon">
-                        <md-icon>visibility</md-icon>
-                        <md-icon slot="selected">visibility_off</md-icon>
-                    </md-icon-button>
-                    </md-outlined-text-field>
+                    <OutlinedTextFieldStyled 
+                        label="Password" 
+                        type="password"
+                    >
+                        <md-icon-button toggle slot="trailing-icon">
+                            <md-icon>visibility</md-icon>
+                            <md-icon slot="selected">visibility_off</md-icon>
+                        </md-icon-button>
+                    </OutlinedTextFieldStyled>
                 </form>
-                            
-                <md-filled-button>Entrar</md-filled-button>
-            </TextContainer>
-            
-            <ImageContainer>
-                <img src={ backgroundLogin } alt="" />
-            </ImageContainer>
 
+                <FilledButtonStyled>Entrar</FilledButtonStyled>
+            </TextContainer>
+
+            <ImageContainer>
+                <img src={backgroundLogin} alt="Login background" />
+            </ImageContainer>
         </Container>
-    )
+    );
 }
--- src/pages/Login/styles.ts
+++ src/pages/Login/styles.ts
@@ -3,7 +3,7 @@ import styled from "styled-components"
 export const Container = styled.div`
   display: flex;
   height: 100vh; /* Ocupa a tela inteira */
-`;
+`
 
 export const TextContainer = styled.div`
   width: 45%;
@@ -17,8 +17,7 @@ export const TextContainer = styled.div`
     font-size: 5rem;
     text-shadow: 2px 2px 4px ${(props) => props.theme['gray-600']};
   }
-
-`;
+`
 
 export const ImageContainer = styled.div`
   width: 55%;
@@ -29,4 +28,22 @@ export const ImageContainer = styled.div`
     height: 100%;
     object-fit: cover;
   }
-`;
+`
+
+
+// Adiciona a tipagem do Web Component, incluindo as propriedades customizadas
+export const OutlinedTextFieldStyled = styled('md-outlined-text-field')<{
+  label?: string;
+  error?: boolean;
+  "error-text"?: string;
+  required?: boolean;
+  type?: string;
+  children?: React.ReactNode;
+}>`
+  --md-outlined-text-field-container-shape: 20px;
+`
+
+// Web Component estilizado: Button
+export const FilledButtonStyled = styled('md-filled-button')`
+  --md-filled-button-container-shape: 20px; 
+`
