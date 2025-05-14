ade9293487c5fb499d880e17925657ff6ebc2366
YasminZanonBarg <yasmin.barg@hotmail.com>
2025-05-13 21:00:25 -0300

    bug 6 - include button v1

--- src/components/IncludeButton/styles.ts
+++ src/components/IncludeButton/styles.ts
@@ -2,21 +2,27 @@ import styled from "styled-components";
 
 interface IncludeButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
 
-// Definindo o estilo diretamente no styled-component para o Web Component
 export const IncludeButtonContainer = styled("md-outlined-button")<IncludeButtonProps>`
-  --md-filled-button-container-shape: 30px;
+  --md-outlined-button-outline-width: 0; /* remove a borda */
+  --md-outlined-button-outline-color: transparent;
+  --md-outlined-button-container-shape: 30px;
   --md-sys-color-primary: ${(props) => props.theme["green-500"]};
   --md-sys-color-on-primary: ${(props) => props.theme["green-300"]};
+  background: ${(props) => props.theme["green-300"]};
+  font-size: 1rem;
   width: 100%;
   max-width: 7rem;
   height: 3rem;
-  background: ${(props) => props.theme["green-300"]};
-  font-size: 1rem;
-  
+
+  display: flex;
+  align-items: center;
+  justify-content: center; /* centraliza horizontalmente */
 
   span {
     display: flex;
     align-items: center;
+    justify-content: center;
+    gap: 0.3rem; /* espaço entre o ícone e o texto */
     white-space: nowrap;
   }
 
